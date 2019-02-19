2a77f0d95f2279317cfdc7b114e83961999e5e4d
Kuga Kugaraja <kuga@Kugas-MacBook-Pro.local>
2019-02-19 14:24:39 -0500

    added express-handlebars

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require("path");
 
 
 const app = express();
 
-// Index Route
-app.get('/', (req, res) => {
-  res.send('INDEX');
+// Handle bar middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.get("/", (req, res) => {
+  const title = "Welcome";
+  res.render("index", {
+    title: title
+  });
 });
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = 5050;
 
