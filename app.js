17189873783f2b1c2b884a77f685cbd49af0ee32
Kuga Kugaraja <kuga@Kugas-MacBook-Pro.local>
2019-02-19 16:35:31 -0500

    added body parser and processed input from form

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require("path");
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
 
 const app = express();
+
+// Load routes
+const guests = require("./routes/guests");
 
 //DB Config
 const db = require('./config/database')
@@ -30,6 +34,10 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   const title = "Welcome";
   res.render("index", {
@@ -37,10 +45,42 @@ app.get("/", (req, res) => {
   });
 });
 
+// Process Form
+app.post("/",  (req, res) => {
+  let errors = [];
+  
+  if (errors.length > 0) {
+    res.render("/", {
+      errors: errors,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      company_name: req.body.company_name,
+      receive_email: req.body.receive_email
+    });
+  } else {
+    const newGuest = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      company_name: req.body.company_name,
+      receive_email: req.body.receive_email,
+    };
+    new Guest(newGuest).save().then(idea => {
+      // req.flash("success_msg", "Thank you for registering! Enjoy the Dinner!");
+      res.redirect("/");
+    });
+  }
+});
+
+
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = 5050;
+
+//Use routes
+app.use("/guests", guests);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
---
+++ routes/guests.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+// Loead Idea Model
+require("../models/Guest");
+const Guest = mongoose.model("guests");
+
+// Process Form
+router.post("/",  (req, res) => {
+  let errors = [];
+  
+  if (errors.length > 0) {
+    res.render("/", {
+      errors: errors,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      company_name: req.body.company_name,
+      receive_email: req.body.receive_email
+    });
+  } else {
+    const newGuest = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      company_name: req.body.company_name,
+      receive_email: req.body.receive_email,
+    };
+    new Guest(newGuest).save().then(idea => {
+      // req.flash("success_msg", "Thank you for registering! Enjoy the Dinner!");
+      res.redirect("../index");
+    });
+  }
+});
+
+module.exports = router;
