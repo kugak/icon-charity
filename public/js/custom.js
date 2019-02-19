597655c1bafc9fde5e7a27215116205ec5252528
Kuga Kugaraja <kuga@Kugas-MacBook-Pro.local>
2019-02-19 14:33:07 -0500

    added layout and required assets

---
+++ public/js/custom.js
@@ -0,0 +1,101 @@
+(function ($) {
+
+  "use strict";
+
+  /*
+   * preloader
+   * -----------------------------------------------------------------------------
+   */
+
+  $(window).on('load', function () {
+      $('.preloader').delay(50).fadeOut(100);
+  });
+
+  /*
+   * Ajax registration form
+   * -----------------------------------------------------------------------------
+   */
+
+  // request submission
+  var EnableDisableForm = function (objectType, btn1, btn1Text) {
+      if (objectType == 'Disable') {
+          $('#' + btn1).attr('disabled', 'disabled');
+      } else {
+          $('#' + btn1).removeAttr('disabled');
+      }
+      $('#' + btn1).val(btn1Text);
+  }
+
+
+  function AjaxFormSubmit(formname, btn1, btn1Text) {
+      var options = {
+          complete: function (response) {
+              if ('Success') {
+                  $('#registerForm').append("<div class='alert alert-success animated fadeInRight' id='success'><strong>Success!</strong> Your registration has been successfully submitted</div>");
+                  setTimeout(function () {
+                      $('#success').addClass('animated fadeOutRight');
+                      $('#success').removeClass('fadeInRight');
+
+                  }, 5000);
+
+                  $('#registerForm')[0].reset();
+              }
+              EnableDisableForm('Enabled', btn1, btn1Text);
+          },
+          error: function (response) {
+              var data = response.responseText;
+              console.log(data);
+          }
+      };
+      $('#' + formname).ajaxSubmit(options);
+  }
+
+
+// Ajax form validation
+  $('#registerForm').validate({
+      errorElement: 'span',
+      rules: {
+          first_name: {
+              required: true
+          },
+          last_name: {
+              required: true
+          },
+          email_address: {
+              required: true
+          },
+          company_name: {
+              required: true
+          },
+          accept_email: {
+              required: true
+          }
+
+      },
+      messages: {
+          first_name: {
+              required: 'First Name is Required.'
+          },
+          last_name: {
+              required: 'Last Name is Required.'
+          },
+          email_address: {
+              required: 'E-Mail Address Required.'
+          },
+          company_name: {
+              required: 'Company Name is Required.'
+          },
+          accept_email: {
+              required: 'Required.'
+          }
+
+      },
+      submitHandler: function (form) {
+          EnableDisableForm('Disable');
+          AjaxFormSubmit('registerForm', 'submit');
+          return false; // required to block normal submit since you used ajax
+      }
+  });
+
+
+})(jQuery);
